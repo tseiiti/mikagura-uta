90b6d4367b8fc9b77b00e3cac01d652ba8c6090b
seiiti <tseiiti@gmail.com>
2025-12-15 19:54:37 +0000

    navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    data = JSON.parse(File.read("public/hymn_data.json"))
+    @hymns = data["hymns"]
+    @instruments = data["instruments"]
   end
 end
