69b9062f103bc458ff8a4789d65c9b67bec90888
seiiti <tseiiti@gmail.com>
2025-12-18 21:58:40 +0000

    start

--- app/javascript/controllers/hymn_controller.js
+++ app/javascript/controllers/hymn_controller.js
@@ -197,6 +197,20 @@ export default class extends Controller {
    * play fill
    */
 
+  start_prm() {
+    prm.id   = 0; // id do interval
+    prm.ini  = 0; // id do tempo (beat) inicial
+    prm.cur  = 0; // id do tempo (beat) atual
+    prm.sts  = 0; // 0: inicio, 1: iniciando (3 tempos), 2: tocando, 3: parado
+    prm.flag = 0;
+    prm.qs_0 = 1;
+    prm.qs_1 = 1;
+    prm.qs_2 = 1;
+    prm.qs_3 = 0;
+    prm.beat = qsa('progress.beat');
+    prm.blen = qsa('progress.beat:not(.d-none)').length;
+  }
+
   track() {
     if (prm.flag == 1 && prm.qs_3 == 0) {
       this.get_html(); // reset
@@ -217,23 +231,23 @@ export default class extends Controller {
       this.play_suwari_aux_2();
     }
     this.track_icon(1);
-    // let e = this.tempos[prm.cur]
-    // let es = qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`)
-    // es[0].scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" })
+    let e = prm.beat[prm.cur];
+    let es = qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`);
+    es[0].scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" });
     
-    // if (this.anima) es[0].style.color = '#555'
-    // let i = 1
-    // prm.id = setInterval(function(fx) {
-    //   if (i == 3) {
-    //     clearInterval(fx.interval)
-    //     prm.ini = prm.cur
-    //     fx.play()
-    //     return
-    //   }
-    //   e = es[i]
-    //   if (fx.anima) e.style.color = '#555'
-    //   i += 1
-    // }, 1000 * 60 / this.cg('bpm'), this);
+    if (config.animation) es[0].style.color = '#555'
+    let i = 1;
+    prm.id = setInterval(function() {
+      if (i == 3) {
+        clearInterval(prm.id);
+        prm.ini = prm.cur;
+        this.play();
+        return;
+      }
+      e = es[i];
+      if (config.animation) e.style.color = '#555';
+      i += 1;
+    }, 1000 * 60 / config.bpm_time);
   }
 
   play_suwari_aux_1() {
@@ -262,20 +276,6 @@ export default class extends Controller {
     e.classList.remove('d-none')
   }
 
-  start_prm() {
-    prm.id   = 0; // id do interval
-    prm.ini  = 0; // id do tempo (beat) inicial
-    prm.cur  = 0; // id do tempo (beat) atual
-    prm.sts  = 0; // 0: inicio, 1: iniciando (3 tempos), 2: tocando, 3: parado
-    prm.flag = 0;
-    prm.qs_0 = 1;
-    prm.qs_1 = 1;
-    prm.qs_2 = 1;
-    prm.qs_3 = 0;
-    prm.beat = qsa('progress.beat');
-    prm.blen = qsa('progress.beat:not(.d-none)').length;
-  }
-
 
   // enfase_aux(e, f) {
   //   if (f) {
@@ -287,8 +287,8 @@ export default class extends Controller {
   // }
 
   // enfase(f) {
-  //   if (!this.anima) return
-  //   let e = this.tempos[prm.cur]
+  //   if (!config.animation) return
+  //   let e = prm.beat[prm.cur]
   //   if (!e) return
   //   e = e.parentElement.querySelector('.texto')
   //   if (e) this.enfase_aux(e, f)
@@ -315,7 +315,7 @@ export default class extends Controller {
 
 
   // play_suwari() {
-  //   let e = this.tempos[prm.cur]
+  //   let e = prm.beat[prm.cur]
   //   if (this.qs_0 < this.gs(0) && e.dataset.paragraph == 0) {
   //     this.play_suwari_aux_1()
   //     this.qs_0 += 1
@@ -343,7 +343,7 @@ export default class extends Controller {
   // }
 
   // play_aux() {
-  //   let e = this.tempos[prm.cur]
+  //   let e = prm.beat[prm.cur]
   //   if (!e) return
   //   let lin = qs(`.paragraph_${e.dataset.paragraph} .line_${e.dataset.line}`)
   //   lin.scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" })
@@ -364,9 +364,9 @@ export default class extends Controller {
   // play() {
   //   this.track_icon(2)
 
-  //   let e = this.tempos[prm.cur]
+  //   let e = prm.beat[prm.cur]
   //   qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`)
-  //     .forEach(f => { if (this.anima) f.style.color = '#555' })
+  //     .forEach(f => { if (config.animation) f.style.color = '#555' })
 
   //   if (this.cg('id') == 's') {
   //     let es = qsa('.mensagem p')
@@ -379,14 +379,14 @@ export default class extends Controller {
   //   if (e.classList.contains('d-none')) prm.cur += 1
   //   this.enfase(true)
 
-  //   let delay = 100 * 60 / this.cg('bpm')
+  //   let delay = 100 * 60 / config.bpm_time
   //   prm.id = setInterval(function(fx) {
   //     let e = prm.beat[prm.cur]
   //     if (!e) {
   //       fx.track_icon(3)
   //       return
   //     }
-  //     if (fx.anima) e.value = fx.valor
+  //     if (config.animation) e.value = fx.valor
 
   //     fx.valor += 1
   //     if (fx.valor > 5) {
