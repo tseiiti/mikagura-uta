bea26ae449c10dd82bcbd092460d37ce521eba65
seiiti <tseiiti@gmail.com>
2025-12-19 14:43:33 +0000

    correção

--- app/javascript/controllers/hymn_controller.js
+++ app/javascript/controllers/hymn_controller.js
@@ -202,33 +202,40 @@ export default class extends Controller {
     prm.id   = 0; // id do interval
     prm.ini  = 0; // id de beat inicial
     prm.cur  = 0; // id de beat atual
+    // prm.cur  = 143; // id de beat atual
     prm.sts  = 0; // status
-    prm.flag = 0; // flag para reinicio
-    prm.qs_0 = 1;
-    prm.qs_1 = 1;
-    prm.qs_2 = 1;
-    prm.qs_3 = 0;
-    prm.beat = qsa('progress.beat');
+    prm.flag = false; // flag para reinicio
+    prm.qs_0 = 0;
+    prm.qs_1 = 0;
+    prm.qs_2 = 0;
+    prm.fsw  = false; // flag suwari término
+    prm.beat = qsa('progress.beat'); // todos os beats
     prm.blen = qsa('progress.beat:not(.d-none)').length;
+    this.bd();
     
     if (config.hymn_id == 'hymn_st') {
-      let es = qsa('.message p')
-      es[0].innerText = `0 de ${ config.suwari_0 } vezes`
-      es[1].innerText = `0 de 1 vez`
-      es[2].innerText = `0 de ${ config.suwari_1 } vezes ( de 0 de ${ config.suwari_2 } )`
+      this.suwari_message();
     }
-
-    // if (config.hymn_id == 'hymn_st') {
-    //   let es = qsa('.message p');
-    //   es[0].innerText = `${ prm.qs_0 } de ${ config.suwari_0 } vezes`;
-    //   es[1].innerText = `${ e.dataset.paragraph >= 1 ? 1 : 0 } de 1 vez`;
-    //   if (e.dataset.paragraph == 2)
-    //     es[2].innerText = `${ prm.qs_1 } de ${ config.suwari_1 } vezes (de ${ prm.qs_2 } de ${ config.suwari_2 })`;
-    // }
   }
 
+  // avança um beat
+  bp() {
+    prm.cur += 1;
+    this.bd();
+  }
+
+  // define beat atual
+  bd() {
+    prm.b = prm.beat[prm.cur];         // beat atual
+    if (prm.b) {
+      prm.p = prm.b.dataset.paragraph; // parágrafo do beat atual
+      prm.l = prm.b.dataset.line;      // linha do beat atual
+    }
+  }
+
+  // controle geral de execução
   track() {
-    if (prm.flag == 1 && prm.qs_3 == 0) {
+    if (prm.flag && !prm.fsw) {
       this.get_html(); // reset
       return
     }
@@ -241,87 +248,120 @@ export default class extends Controller {
     }
   }
 
+  // 3 tempos iniciais
   start() {
-    if (prm.qs_3 == 1) {
+    if (prm.fsw) {
       this.play_suwari_aux_1();
       this.play_suwari_aux_2();
     }
     this.track_icon(1);
-    let e = prm.beat[prm.cur];
-    let es = qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`);
+
+    // scroll na linha
+    let es = qsa(`.first-span.paragraph_${ prm.p }.line_${ prm.l } span`);
     es[0].scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" });
     
-    if (config.animation) es[0].style.color = '#555'
-    let i = 1;
+    // troca cor do ponto no tempo
+    let i = 0;
+    if (config.animation) es[i].style.color = '#555'
     prm.id = setInterval(function(fx) {
+      i += 1;
       if (i == 3) {
         clearInterval(prm.id);
         prm.ini = prm.cur;
         fx.play();
         return;
       }
-      e = es[i];
-      if (config.animation) e.style.color = '#555';
-      i += 1;
+      if (config.animation) es[i].style.color = '#555';
     }, 1000 * 60 / config.bpm_time, this);
   }
 
+  // tocar
   play() {
     this.track_icon(2);
 
-    let e = prm.beat[prm.cur];
-    qsa(`.first-span.paragraph_${e.dataset.paragraph}.line_${e.dataset.line} span`)
+    // marca 3 pontos iniciais
+    qsa(`.first-span.paragraph_${ prm.p }.line_${ prm.l } span`)
       .forEach(f => { if (config.animation) f.style.color = '#555' });
 
-    this.suwari_message(e);
+    // pula ocultos iniciais do parágrafo
+    if (prm.b.classList.contains('d-none')) this.bp();
 
-    if (e.classList.contains('d-none')) prm.cur += 1;
+    this.suwari_message();
+    
+    let i = 0;
     this.enfase(true);
-
-    let i = 1;
     prm.id = setInterval(function(fx) {
-      let e = prm.beat[prm.cur];
-      if (!e) {
-        fx.track_icon(3);
-        return;
-      }
-      if (config.animation) e.value = i;
       i += 1;
-      if (i > 5) {
+      if (config.animation) prm.b.value = i;
+
+      if (i == 5) {
+        i = 0;
         fx.enfase(false)
-        i = 1;
-        prm.cur += 1;
         fx.play_aux();
       }
     }, 100 * 60 / config.bpm_time, this);
   }
 
-  play_suwari() {c(0)
-    let e = prm.beat[prm.cur];
-    if (prm.qs_0 < config.suwari_0 && e.dataset.paragraph == 0) {
-      this.play_suwari_aux_1();
-      prm.qs_0 += 1;
-      qsa('.message p')[0].innerText = `${ prm.qs_0 } de ${ config.suwari_0 } vezes`;
-      return true
-    }
-
-    if (prm.qs_1 < config.suwari_1 && e.dataset.paragraph == 2) {
-      this.play_suwari_aux_1();
-      prm.qs_1 += 1;
-      qsa('.message p')[2].innerText = `${ prm.qs_1 } de ${ config.suwari_1 } vezes (de ${ prm.qs_2 } de ${ config.suwari_2 })`;
-      return true;
-    }
-
-    prm.qs_3 = 0;
-    if (prm.qs_2 < config.suwari_2 && e.dataset.paragraph == 2) {
-      prm.qs_1 = 1;
-      prm.qs_2 += 1;
-      prm.qs_3 = 1;
+  // execução de cada beat
+  play_aux() {
+    // término
+    this.bp();
+    if (!prm.b) {
+      this.track_icon(3);
+      return;
     }
     
-    qsa('.message p')[1].innerText = `${ e.dataset.paragraph >= 1 ? 1 : 0 } de 1 vez`;
-    this.play_suwari_aux_3();
-    return false;
+    let lin = qs(`.paragraph_${ prm.p } .line_${ prm.l }`);
+    lin.scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" });
+    let es = lin.querySelectorAll('.beat');
+    if (es[es.length - 1] == prm.b) {
+      // pausa para início de 3 tempos
+      if (lin.classList.contains('pause')) {
+        if (this.play_suwari()) return;
+        this.track_icon(0);
+        this.bp();
+      }
+    }
+    this.enfase(true);
+  }
+
+  // mensagens do suwari
+  suwari_message() {
+    if (config.hymn_id == 'hymn_st') {
+      if (prm.qs_0 == 0 && prm.cur > 0) prm.qs_0 = 1;
+      if (prm.qs_1 == 0 && prm.p > 1) prm.qs_1 = prm.qs_2 = 1;
+
+      let es = qsa('.message p');
+      es[0].innerText = `${ prm.qs_0 } de ${ config.suwari_0 } vezes`;
+      es[1].innerText = `${ prm.p >= 1 ? 1 : 0 } de 1 vez`;
+      es[2].innerText = `${ prm.qs_1 } de ${ config.suwari_1 } vezes (de ${ prm.qs_2 } de ${ config.suwari_2 })`;
+    }
+  }
+
+  // parada suwari
+  play_suwari() {
+    if (config.hymn_id != 'hymn_st') return false;
+
+    let boolean = false;
+    if (prm.qs_0 < config.suwari_0 && prm.p == 0) {
+      this.play_suwari_aux_1();
+      prm.qs_0 += 1;
+      boolean = true;
+    } else if (prm.qs_1 < config.suwari_1 && prm.p == 2) {
+      this.play_suwari_aux_1();
+      prm.qs_1 += 1;
+      boolean = true;
+    } else {
+      prm.fsw = false;
+      if (prm.qs_2 < config.suwari_2 && prm.p == 2) {
+        prm.qs_1  = 1;
+        prm.qs_2 += 1;
+        prm.fsw   = true;
+      }
+      this.play_suwari_aux_3();
+    }
+    this.suwari_message();
+    return boolean;
   }
 
   play_suwari_aux_1() {
@@ -329,16 +369,15 @@ export default class extends Controller {
       prm.beat[i].value = 0;
     }
     prm.cur = prm.ini;
-    let e = prm.beat[prm.cur];
-    e.classList.remove('d-none');
-    e = qs(`.first-span.paragraph_${ e.dataset.paragraph }.line_${ e.dataset.line }`);
+    this.bd();
+    prm.b.classList.remove('d-none');
+    let e = qs(`.first-span.paragraph_${ prm.p }.line_${ prm.l }`);
     e.classList.add('d-none');
   }
 
   play_suwari_aux_2() {
-    let e = prm.beat[prm.cur];
-    e.classList.add('d-none');
-    e = qs(`.first-span.paragraph_${ e.dataset.paragraph }.line_${ e.dataset.line }`);
+    prm.b.classList.add('d-none');
+    let e = qs(`.first-span.paragraph_${ prm.p }.line_${ prm.l }`);
     e.classList.remove('d-none');
     e.querySelectorAll('span').forEach(f => { f.style.color = '#ccc' });
   }
@@ -352,7 +391,7 @@ export default class extends Controller {
 
   enfase(f) {
     if (!config.animation) return;
-    let e = prm.beat[prm.cur];
+    let e = prm.b;
     if (!e) return;
     e = e.parentElement.querySelector('.part_text');
     if (e) {
@@ -361,6 +400,7 @@ export default class extends Controller {
     }
   }
 
+  // status e ícone
   track_icon(sts) {
     // 0: inicio, 1: iniciando (3 tempos), 2: tocando, 3: parado
     prm.sts = sts;
@@ -377,35 +417,7 @@ export default class extends Controller {
     });
 
     if (prm.cur >= prm.blen) {
-      prm.flag = 1;
-    }
-  }
-
-  play_aux() {
-    let e = prm.beat[prm.cur];
-    if (!e) return;
-    let lin = qs(`.paragraph_${e.dataset.paragraph} .line_${e.dataset.line}`);
-    lin.scrollIntoView({ behavior: "smooth", block: 'center', inline: "nearest" });
-    let es = lin.querySelectorAll('.beat');
-    if (es[es.length - 1] == e) {
-      this.enfase(false);
-
-      if (lin.classList.contains('pause')) {
-        if (config.hymn_id == 'hymn_st' && this.play_suwari()) return;
-        this.track_icon(0);
-        prm.cur += 1;
-      }
-    }
-    this.enfase(true);
-  }
-
-  suwari_message(e) {
-    if (config.hymn_id == 'hymn_st') {
-      let es = qsa('.message p');
-      es[0].innerText = `${ prm.qs_0 } de ${ config.suwari_0 } vezes`;
-      es[1].innerText = `${ e.dataset.paragraph >= 1 ? 1 : 0 } de 1 vez`;
-      if (e.dataset.paragraph == 2)
-        es[2].innerText = `${ prm.qs_1 } de ${ config.suwari_1 } vezes (de ${ prm.qs_2 } de ${ config.suwari_2 })`;
+      prm.flag = true;
     }
   }
 }
